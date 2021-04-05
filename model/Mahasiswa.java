f24ddaf66e6692fb50031a8f8b7251a3afc599bb
izai28 <Herromax598@gmail.com>
2021-04-05 16:05:26 +0800

    Memanggil method dari banyak class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("======Data Mahasiswa======");
+  System.out.println("NPM  : 19630311");
+  System.out.println("Nama : Muhammad Zaini");
+  System.out.println("===========================");
+
+ }
+}
+
---
+++ model/MataKuliah.java
@@ -0,0 +1,12 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("======Data MataKuliah======");
+  System.out.println("Kode  : 1TIF3402");
+  System.out.println("Nama  : Pemograman Berbasis Objek 1");
+  System.out.println("===========================");
+
+ }
+}
